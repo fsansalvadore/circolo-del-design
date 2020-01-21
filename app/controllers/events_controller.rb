111c7003aacb97f96184ab9fdf2544f67588cbe2
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-01-21 18:27:53 +0100

    added events and calendar page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_superpower, only: [:show, :edit, :update, :destroy]
+  # before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
     @events = Event.all
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get    'events/new', to: 'events#new'
   post   'events', to: 'events#create'
 
-  get    'events/:id', to: 'events#show', as: :event
+  # get    'events/:id', to: 'events#show', as: :event
 
   get    'events/:id/edit', to: 'events#edit', as: 'edit_event'
   patch  'events/:id', to: 'events#update'
