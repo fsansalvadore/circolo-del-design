0f5b754f71a2275d273a0839f21bb16af8df5cfe
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-01-21 13:48:58 +0100

    deleted popup js

--- app/assets/javascripts/event-popup.js
+++
@@ -1,9 +0,0 @@
-const popup = $(".event-popup-details-container");
-
-$(window).on('scroll', function () {
-    if ($(window).scrollTop() >= ($('#main-content').offset().top) - $('.navbar-inner-page').height()) {
-        popup.addClass('popup-open');
-    } else {
-        popup.removeClass('popup-open');
-    }
-});
