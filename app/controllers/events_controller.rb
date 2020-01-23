124614f276a6bc456737224410358a2f38d77cc1
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-01-23 15:58:40 +0100

    calendar dynamically populated by db and 'published' events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
-    @events = Event.all
+    @events = Event.all.where(published: true)
   end
 
   def show
