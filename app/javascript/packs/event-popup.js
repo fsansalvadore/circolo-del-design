a273296efc47900e5fc645a1d0a9ed9c5b8870d5
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-01-23 21:53:46 +0100

    fixed event popup not showing

---
+++ app/javascript/packs/event-popup.js
@@ -0,0 +1,14 @@
+const Popup = () => {
+
+const popup = $(".event-popup-details-container");
+
+  $(window).on('scroll', function () {
+      if ($(window).scrollTop() >= ($('#main-content').offset().top) - $('.navbar-inner-page').height()) {
+          popup.addClass('popup-open');
+      } else {
+          popup.removeClass('popup-open');
+      }
+  });
+}
+
+export { Popup };
