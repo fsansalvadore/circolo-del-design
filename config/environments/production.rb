6ade502f5d561772cd5f0b0a0d8b82b610517570
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-02-03 18:53:02 +0100

    changed domain ActionMailer domain config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'yourdomain.com',
+    :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
