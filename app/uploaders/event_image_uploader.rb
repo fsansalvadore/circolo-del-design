5cb872643e7398d9962e15692ba4089ec7215cb7
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-02-06 00:07:48 +0100

    changed cloudinary directory to cdd

--- app/uploaders/event_image_uploader.rb
+++ app/uploaders/event_image_uploader.rb
@@ -7,7 +7,9 @@ class EventImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
-
+  def public_id
+    return "circolo-del-design/" + Cloudinary::Utils.random_public_id;
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   # def store_dir
