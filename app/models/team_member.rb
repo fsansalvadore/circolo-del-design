8374e8ffdab4fcae90807c2efac6ca91487e3d96
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-02-15 16:31:55 +0100

    made team members reorderable by drag and drop

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -2,4 +2,5 @@ class TeamMember < ApplicationRecord
   validates :nome, :cognome, :ruolo, presence: true
 
   mount_uploader :image, TeamMemberImageUploader
+  acts_as_list
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_112924) do
+ActiveRecord::Schema.define(version: 2020_02_15_150425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2020_02_15_112924) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.boolean "published", default: false
+    t.integer "position"
   end
 
   create_table "users", force: :cascade do |t|
