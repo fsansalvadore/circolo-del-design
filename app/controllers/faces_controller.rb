79eb3bfc063e6b6b5b5cbf68de1799e322df03b8
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-02-27 18:14:10 +0100

    fixed things

--- app/controllers/faces_controller.rb
+++ app/controllers/faces_controller.rb
@@ -1,4 +1,6 @@
 class FacesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
     @faces = Face.where(published: true).order(:cognome)
   end
