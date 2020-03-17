0a9396940ccd1e55c3cbb88147c7ccaf5c1cc816
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-03-17 22:16:28 +0100

    updated responsive blog columns nav

--- app/javascript/packs/blog-nav.js
+++ app/javascript/packs/blog-nav.js
@@ -4,6 +4,10 @@ const BlogNav = () => {
   if (blogNav.length > 0) {
     const navbar = $('header.navbar-inner-page');
     const header = $('header#top');
+    const navBtn = document.querySelector('a#blog_nav_btn');
+    const navContainer = $('.blog_nav_container');
+    const navLinks = $('#blog_nav_links');
+    const navLinksGhost = $('#blog_nav_links_ghost');
 
     $(window).on('scroll', function () {
         let main = $('main.page-postheader');
@@ -14,6 +18,26 @@ const BlogNav = () => {
             blogNav.removeClass('fixed');
             blogNav.css("top", header.outerHeight() + "px");
         }
+    });
+
+    navBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      navContainer.toggleClass('open');
+
+      if(navContainer.hasClass('open')) {
+        navLinks.css("display", "block");
+        navLinks.css('height', navLinksGhost.outerHeight() + 'px');
+        setTimeout(() => {
+          navLinks.addClass('show');
+        }, 100);
+      } else {
+        navLinks.css('opacity', '0px');
+        setTimeout(() => {
+        navLinks.css('height', '0px');
+          navLinks.removeClass('show');
+        }, 100)
+        // navLinks.css('display', "none");
+      }
     });
   }
 }
