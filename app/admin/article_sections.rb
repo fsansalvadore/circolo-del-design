9960ee054d923b75570877a92ec2c8768617a43d
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-04-20 13:53:40 +0200

    updated order position for blog_post sections

--- app/admin/article_sections.rb
+++ app/admin/article_sections.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register ArticleSection do
   belongs_to :article
 
-  config.sort_order = 'position'
+  config.sort_order = 'position_asc'
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
