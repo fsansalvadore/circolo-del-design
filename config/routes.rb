30f678ff4b74c1c27b7b490e7dc986011bb66c19
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2021-02-10 18:46:47 +0100

    home page fix

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
-  get     'homepage', to: 'pages#homepage'
+
   # resources :contacts, only: [:index, :new, :create]
   get    'contatti',      to: 'contacts#index'
   post   'contatti',      to: 'contacts#create'
