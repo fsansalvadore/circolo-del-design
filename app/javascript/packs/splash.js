8ed19242a845ae75afa099808f34f025c7504756
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2021-02-11 11:38:20 +0100

    splash js fix

--- app/javascript/packs/splash.js
+++ app/javascript/packs/splash.js
@@ -1,13 +1,15 @@
 const Splash = () => {
-  document.querySelector("#splash.splash-container").addEventListener("click", (e) => {
-    e.target.classList.add("splash-out");
-    console.log("e.target", e.target)
-    console.log("e.target.classList", e.target.classList)
-
-    setTimeout(() => {
-      e.target.remove();
-    }, 1000);
-  });
+  if(document.querySelector("#splash.splash-container")) {
+    document.querySelector("#splash.splash-container").addEventListener("click", (e) => {
+      e.target.classList.add("splash-out");
+      console.log("e.target", e.target)
+      console.log("e.target.classList", e.target.classList)
+  
+      setTimeout(() => {
+        e.target.remove();
+      }, 1000);
+    });
+  }
 }
 
 export { Splash };
