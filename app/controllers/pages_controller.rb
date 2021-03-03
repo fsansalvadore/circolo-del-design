5eca657c0e4a7d6af50f6462e83d23cca2c797f8
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2021-03-03 15:36:09 +0100

    home page go live

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,41 +16,12 @@ class PagesController < ApplicationController
   def index
     @events = Event.where('data_fine >= ? OR data_inizio >= ?', DateTime.now, DateTime.now + 20).where("published = true").limit(5).sort_by{ |e| [e.priority, e.data_inizio] }
     @special_projects = SpecialProject.where("published = true").limit(5).order(year: :desc)
-    # @blog_posts = BlogPost.where("published = true AND priority BETWEEN 1 AND 4").limit(5).sort_by{ |e| [e.priority, e.created_at] }
-    # @articles = Article.where("published = true AND priority BETWEEN 1 AND 4").limit(5).sort_by{ |e| [e.priority, e.created_at] }
-    # @slider_cover = Article.where("priority = 6 AND published = true").first
-    @wpac = WpacSection.all.first
-    @page = PageHome.all.first
-    # @slider_cover = BlogPost.where("priority = 6 AND published = true").first
-    render :layout => 'home'
-  end
-  
-  def home_page_staging
-    @events = Event.where('data_fine >= ? OR data_inizio >= ?', DateTime.now, DateTime.now + 20).where("published = true").limit(5).sort_by{ |e| [e.priority, e.data_inizio] }
-    @special_projects = SpecialProject.where("published = true").limit(5).order(year: :desc)
-    # @blog_posts = BlogPost.where("published = true AND priority BETWEEN 1 AND 4").limit(5).sort_by{ |e| [e.priority, e.created_at] }
-    # @articles = Article.where("published = true AND priority BETWEEN 1 AND 4").limit(5).sort_by{ |e| [e.priority, e.created_at] }
-    # @slider_cover = Article.where("priority = 6 AND published = true").first
-    @wpac = WpacSection.all.first
-    @page = PageHome.all.first
-    # @slider_cover = BlogPost.where("priority = 6 AND published = true").first
-    render :layout => 'home'
-  end
-
-  def home_page_staging
-    @events = Event.where('data_fine >= ? OR data_inizio >= ?', DateTime.now, DateTime.now + 20).where("published = true").limit(5).sort_by{ |e| [e.priority, e.data_inizio] }
-    @special_projects = SpecialProject.where("published = true").limit(5).order(year: :desc)
-    # @blog_posts = BlogPost.where("published = true AND priority BETWEEN 1 AND 4").limit(5).sort_by{ |e| [e.priority, e.created_at] }
-    # @articles = Article.where("published = true AND priority BETWEEN 1 AND 4").limit(5).sort_by{ |e| [e.priority, e.created_at] }
-    # @slider_cover = Article.where("priority = 6 AND published = true").first
-
     @wpac = WpacSection.all.first
     @page = PageHome.all.first
     @cards = HomePageCard.where(page_home_id: @page.id).order(:position)
     @col_1_links = HomePageColumnOneLink.where(page_home_id: @page.id).order(:position)
     @col_2_links = HomePageColumnTwoLink.where(page_home_id: @page.id).order(:position)
     @col_3_links = HomePageColumnThreeLink.where(page_home_id: @page.id).order(:position)
-    # @slider_cover = BlogPost.where("priority = 6 AND published = true").first
     render :layout => 'home'
   end
 
