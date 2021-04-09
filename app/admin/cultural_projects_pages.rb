0007fe8c8ffc899ad1e20f1f2cc39c58fa034967
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2021-04-09 10:50:20 +0200

    feat(content-edit): added content edit feature for pages

--- app/admin/cultural_projects_pages.rb
+++ app/admin/cultural_projects_pages.rb
@@ -9,4 +9,61 @@ ActiveAdmin.register CulturalProjectsPage do
                 :heading,
                 :subheading,
                 :bg_color
+
+  config.comments = false
+  actions :all, :except => [:destroy]
+                
+  controller do
+    def index
+      redirect_to admin_cultural_projects_page_path(CulturalProjectsPage.all.first)
+    end
+  end
+
+  show title: :page_label do
+    columns do
+      column do
+        attributes_table do
+          row (:heading) { |cultural_project| raw(cultural_project.heading) }
+          row (:subheading) { |cultural_project| raw(cultural_project.subheading) }
+        end
+      end
+      column do
+        attributes_table :title => "SEO" do
+          row :meta_title
+          row :meta_description
+          row :meta_keywords
+        end
+      end
+    end
+  end
+
+  # enable image deletion
+  member_action :delete_image, method: :delete do
+    @img = Cloudinary::CarrierWave.find(params[:id])
+    @img.purge_later
+    redirect_back(fallback_location: edit_admin_cultural_projects_page_path)
+  end
+  
+  form do |f|
+    f.actions
+    f.semantic_errors *f.object.errors.keys
+    tabs do
+      tab :heading do
+        f.inputs "Heading" do
+          f.input :page_label, label: "Titolo Pagina", hint: "Compare come titolo della pagina in admin."
+          f.input :heading, label: "Titolo"
+          f.input :subheading, label: "Sottotitolo", as: :quill_editor
+        end
+      end
+      tab :meta do
+        f.inputs 'Meta Data' do
+          f.input :meta_title, label: "Meta Title", placeholder: 'Meta Title'
+          f.input :meta_description, label: "Meta Description", placeholder: 'Meta Description'
+          f.input :meta_keywords, label: "Meta Keywords", placeholder: 'Meta Keywords'
+          f.input :meta_image, as: :file, label: "Meta Image", :image_preview => true
+        end
+      end
+    end
+    f.actions
+  end
 end
