f4a08aebad94eeea8464f2e22217ebdb0a04f09c
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-03-19 18:27:58 +0100

    fix sendgrid mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -21,17 +21,5 @@ class ContactsController < ApplicationController
       flash.now[:error] = 'Impossibile inviare il messaggio al momento.'
       render :index
     end
-
-    #   respond_to do |format|
-    #     @contact = Contact.new
-    #     format.html { render 'index'}
-    #     format.js   { flash.now[:success] = @message = "Grazie per il messaggio! Cercheremo di rispondere in breve tempo." }
-    #   end
-    # else
-    #   respond_to do |format|
-    #     format.html { render 'index' }
-    #     format.js   { flash.now[:error] = @message = "Non è possibile inviare il messaggio al momento." }
-    #   end
-    # end
   end
 end
