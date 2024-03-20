9935ce0c47a3a3e600bbb838a143746965ca999a
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-03-20 18:10:22 +0100

    riprova smpt sendgrid

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -16,26 +16,28 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
 
-    from = Email.new(email: 'hello@fsansalvadore.com')
-    to = Email.new(email: @contact.email)
-    subject = 'Messaggio da form contatti'
-    content = Content.new(type: 'text/plain', value: @contact.messaggio)
-    mail = Mail.new(from, subject, to, content)
+    # from = Email.new(email: 'hello@fsansalvadore.com')
+    # to = Email.new(email: @contact.email)
+    # subject = 'Messaggio da form contatti'
+    # content = Content.new(type: 'text/plain', value: @contact.messaggio)
+    # mail = Mail.new(from, subject, to, content)
 
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_PASSWORD'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.headers
-    # @contact.request = request
-    # if @contact.deliver
-    #   flash[:success] = 'Messaggio inviato con successo'
-    #   flash.now[:error] = nil
-    #   redirect_to contatti_path
-    # else
-    #   flash.now[:error] = 'Impossibile inviare il messaggio al momento.'
-    #   render :index
-    # end
+    # sg = SendGrid::API.new(api_key: ENV['SENDGRID_PASSWORD'])
+    # response = sg.client.mail._('send').post(request_body: mail.to_json)
+    # puts response.status_code
+    # puts response.body
+    # puts response.headers
+    @contact.request = request
+    if @contact.deliver
+      puts "Messaggio inviato con successo"
+      flash[:success] = 'Messaggio inviato con successo'
+      flash.now[:error] = nil
+      redirect_to contatti_path
+    else
+      puts "Errore: Impossibile inviare il messaggio al momento."
+      flash.now[:error] = 'Impossibile inviare il messaggio al momento.'
+      render :index
+    end
   end
 
   private
