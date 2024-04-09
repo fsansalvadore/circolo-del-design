52ed27e8427aee521b88848805a414179380ba03
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-04-09 14:56:52 +0200

    block nickname

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,21 +12,23 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(contact_params)
-    @contact.request = request
+    unless contact_params[:nickname].empty?
+      @contact.request = request
 
-    if @contact.deliver
-      flash.now[:error] = nil
-      flash[:notice] = 'Messaggio inviato con successo'
-      redirect_to contatti_path
-    else
-      flash.now[:error] = 'Impossibile inviare il messaggio al momento.'
-      render :index
+      if @contact.deliver
+        flash.now[:error] = nil
+        flash[:notice] = 'Messaggio inviato con successo'
+        redirect_to contatti_path
+      else
+        flash.now[:error] = 'Impossibile inviare il messaggio al momento.'
+        render :index
+      end
     end
   end
 
   private
 
   def contact_params
-    params.require(:contact).permit(:nome, :email, :messaggio)
+    params.require(:contact).permit(:nome, :email, :messaggio, :nickname)
   end
 end
